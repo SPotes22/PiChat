3757da4ffb66aab4b2183f92b29381fcef2a134e
SPotes22 <potesdev@gmail.com>
2025-08-27 23:24:54 -0500

    LAYER8_bug

--- servidor.py
+++ servidor.py
@@ -247,4 +247,4 @@ if __name__ == '__main__':
 # para Gunicorn / Render / Railway
 application = app
 # No es necesario re-inicializar socketio aquí.
-application = socketio
+# application = socketio
