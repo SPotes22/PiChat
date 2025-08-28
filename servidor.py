29b1d79607d7016b8bb9440e6376d92a01f63489
SPotes22 <potesdev@gmail.com>
2025-08-27 22:34:17 -0500

    eventlet BugFix

--- servidor.py
+++ servidor.py
@@ -19,6 +19,10 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 '''
+# 👇 esto siempre va primero
+import eventlet
+eventlet.monkey_patch()
+
 import os
 import json
 from datetime import timedelta
